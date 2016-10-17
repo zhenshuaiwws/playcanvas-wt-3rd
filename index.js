d1d3c41fceb9d12a6b5f2b511f4dc882d826ff9a
zhenshuai <zhenshuai@worktile.com>
2016-10-17 14:59:43 +0800

    v1.3

--- index.js
+++ index.js
@@ -1,29 +1,84 @@
-var game = init();
-
-function init() {
+function Game() {
     var p = {
         logoHeight : 0.2,
         boxHeight  : 0.5,
-        aimBoxSpeed: 5,
         isCollision: true,
         colors     : [
-            {r: 237, g: 66, b: 87},
-            {r: 239, g: 83, b: 102},
-            {r: 240, g: 94, b: 112},
-            {r: 242, g: 106, b: 124},
-            {r: 244, g: 120, b: 136},
-            {r: 245, g: 132, b: 146},
-            {r: 246, g: 140, b: 154},
-            {r: 247, g: 151, b: 151},
-            {r: 255, g: 226, b: 169}
+            [0.90, 0.26, 0.37],
+            [0.92, 0.29, 0.36],
+            [0.93, 0.32, 0.38],
+            [0.95, 0.36, 0.40],
+            [0.96, 0.42, 0.44],
+
+            [0.97, 0.51, 0.51],
+            [0.97, 0.56, 0.55],
+            [0.98, 0.60, 0.58],
+            [0.98, 0.64, 0.61],
+            [0.98, 0.67, 0.64],
+
+            [0.98, 0.70, 0.66],
+            [0.99, 0.73, 0.69],
+            [0.99, 0.76, 0.71],
+            [0.99, 0.78, 0.73],
+            [1.00, 0.88, 0.75],
+
+            [1.00, 0.84, 0.75],
+            [1.00, 0.81, 0.74],
+            [0.99, 0.79, 0.73],
+            [0.99, 0.76, 0.71],
+            [0.99, 0.74, 0.69],
+
+            [0.98, 0.69, 0.66],
+            [0.98, 0.67, 0.64],
+            [0.98, 0.63, 0.61],
+            [0.98, 0.60, 0.58],
+            [0.97, 0.56, 0.55],
+
+            [0.96, 0.50, 0.49],
+            [0.96, 0.44, 0.45],
+            [0.95, 0.38, 0.41],
+            [0.94, 0.33, 0.38],
+            [0.92, 0.28, 0.36],
+
+            [0.90, 0.26, 0.37],
+            [0.89, 0.25, 0.38],
+            [0.87, 0.24, 0.41],
+            [0.85, 0.24, 0.43],
+            [0.83, 0.24, 0.46],
+
+            [0.80, 0.25, 0.50],
+            [0.78, 0.25, 0.54],
+            [0.76, 0.25, 0.57],
+            [0.74, 0.25, 0.62],
+            [0.72, 0.25, 0.65],
+
+            [0.69, 0.25, 0.69],
+            [0.66, 0.25, 0.72],
+            [0.64, 0.25, 0.75],
+            [0.61, 0.26, 0.76],
+            [0.56, 0.27, 0.79],
+
+            [0.51, 0.27, 0.81],
+            [0.47, 0.28, 0.83],
+            [0.44, 0.29, 0.84],
+            [0.41, 0.29, 0.84],
+            [0.38, 0.31, 0.85],
+
+            [0.35, 0.33, 0.85],
+            [0.32, 0.35, 0.85],
+            [0.30, 0.37, 0.85],
+            [0.27, 0.40, 0.84],
+            [0.25, 0.42, 0.84]
+
         ],
         isGameOver : false
     };
+    this.p = p;
 
-    var boxs = [];
     var model = {
         boxs: []
     };
+    this.model = model;
 
     //region start
     var canvas = document.getElementById("application-canvas");
@@ -45,12 +100,18 @@ function init() {
         material.diffuse.set(r, g, b);
         material.specular.set(0.5, 0.5, 0.5);
         material.shininess = 50;
+
+
         material.update();
         return material;
     }
 
+    function getColor() {
+        return p.colors[model.boxs.length % p.colors.length];
+    }
+
     //region 目标块
-    function createAimBox() {
+    function AimBox() {
         this.dir = 'right';
         this.speed = 5;
 
@@ -59,51 +120,57 @@ function init() {
             type: "box"
         });
         this.entity.setLocalScale(3 + p.logoHeight + p.logoHeight, p.boxHeight, 2 + p.logoHeight);
-
-        this.entity.setPosition(0, boxs.length * p.boxHeight + 3, 0);
-
+        this.entity.setPosition(0, model.boxs.length * p.boxHeight + 3, 0);
+        var _color = getColor();
+        this.entity.model.model.meshInstances[0].material = createMaterial(_color[0], _color[1], _color[2]);
         app.root.addChild(this.entity);
-        return this;
     }
-
-    createAimBox.prototype.resetPos = function (dir) {
+    AimBox.prototype.reset = function () {
+        var _color = getColor();
+        this.entity.model.model.meshInstances[0].material = createMaterial(_color[0], _color[1], _color[2]);
+        this.resetPos();
+    };
+    AimBox.prototype.resetMaterial = function () {
+        var _color = getColor();
+        this.entity.model.model.meshInstances[0].material = createMaterial(_color[0], _color[1], _color[2]);
+    };
+    AimBox.prototype.resetPos = function (dir) {
         this.entity.enabled = true;
         this.dir = dir;
         var _offset = 3;
         if (this.dir == 'right') {
-            this.entity.setPosition(_offset, boxs.length * p.boxHeight + 3, 0);
+            this.entity.setPosition(_offset, model.boxs.length * p.boxHeight + 3, 0);
         } else {
-            this.entity.setPosition(0, boxs.length * p.boxHeight + 3, _offset * -1);
+            this.entity.setPosition(0, model.boxs.length * p.boxHeight + 3, _offset * -1);
         }
     };
-    createAimBox.prototype.update = function (dt) {
+    AimBox.prototype.update = function (dt) {
 
         if (this.dir == 'right') {
             var aimOldPosX = this.entity.getPosition().x;
             if (aimOldPosX > 3) {
-                this.speed = this.speed * -1;
-            }
-            if (aimOldPosX < -3) {
-                this.speed = this.speed * -1;
+                this.speed = Math.abs(this.speed) * -1;
+            } else if (aimOldPosX < -3) {
+                this.speed = Math.abs(this.speed);
             }
             var aimPosX = aimOldPosX + this.speed * dt;
-            this.entity.setPosition(aimPosX, boxs.length * p.boxHeight + 3, 0)
+            this.entity.setPosition(aimPosX, model.boxs.length * p.boxHeight + 3, 0)
         } else {
             var aimOldPosZ = this.entity.getPosition().z;
             if (aimOldPosZ > 3) {
-                this.speed = this.speed * -1;
-            }
-            if (aimOldPosZ < -3) {
-                this.speed = this.speed * -1;
+                this.speed = Math.abs(this.speed) * -1;
+            } else if (aimOldPosZ < -3) {
+                this.speed = Math.abs(this.speed);
             }
             var aimPosZ = aimOldPosZ + this.speed * dt;
-            this.entity.setPosition(0, boxs.length * p.boxHeight + 3, aimPosZ)
+            this.entity.setPosition(0, model.boxs.length * p.boxHeight + 3, aimPosZ)
         }
+
 
     };
 
-    model.aimBox = new createAimBox();
-    model.aimBox.resetPos('right');
+    model.AimBox = new AimBox();
+    model.AimBox.resetPos('right');
 
     //endregion
 
@@ -119,7 +186,7 @@ function init() {
             type: "box"
         });
         if (arg.color) {
-            entity.model.model.meshInstances[0].material = createMaterial(arg.color.r / 255, arg.color.g / 255, arg.color.b / 255);
+            entity.model.model.meshInstances[0].material = createMaterial(arg.color[0], arg.color[1], arg.color[2]);
         }
 
         entity.setLocalScale(_size.x, _size.y, _size.z);
@@ -148,10 +215,11 @@ function init() {
     }
 
     function boxCollisionCallback() {
-        if (model.aimBox.dir == 'right') {
-            model.aimBox.resetPos('left');
+
+        if (model.AimBox.dir == 'right') {
+            model.AimBox.resetPos('left');
         } else {
-            model.aimBox.resetPos('right');
+            model.AimBox.resetPos('right');
         }
     }
 
@@ -159,19 +227,20 @@ function init() {
     window.addEventListener('touchstart', function () {
         if (p.isCollision) {
             p.isCollision = false;
-            if (model.aimBox.entity.enabled == true) {
-                model.aimBox.entity.enabled = false;
-                boxs.push({
-                    index : boxs.length,
+            if (model.AimBox.entity.enabled == true) {
+                model.AimBox.entity.enabled = false;
+                model.boxs.push({
+                    index : model.boxs.length,
                     entity: new Box({
                         pos  : {
-                            x: model.aimBox.entity.getPosition().x,
-                            y: model.aimBox.entity.getPosition().y,
-                            z: model.aimBox.entity.getPosition().z
+                            x: model.AimBox.entity.getPosition().x,
+                            y: model.AimBox.entity.getPosition().y,
+                            z: model.AimBox.entity.getPosition().z
                         },
-                        color: p.colors[boxs.length]
+                        color: getColor()
                     })
                 });
+                model.AimBox.resetMaterial();
             }
         }
 
@@ -195,11 +264,6 @@ function init() {
             type       : "box",
             halfExtents: [p.scale_x / 2, p.scale_y / 2, p.scale_z / 2]
         });
-        // ground.addComponent('light', {
-        //     type: "point",
-        //     color: new pc.Color(1, 0, 0),
-        //     range: 2
-        // });
 
         var material = p.material || createMaterial(100 / 255, 100 / 255, 112 / 255);
         ground.model.model.meshInstances[0].material = material;
@@ -267,24 +331,44 @@ function init() {
 
     //region 摄像机
     function Camera() {
-        var camera = new pc.Entity();
-        camera.addComponent('camera', {
+        this.entity = new pc.Entity();
+        this.entity.addComponent('camera', {
             clearColor: new pc.Color(46 / 255, 42 / 255, 56 / 255)
         });
-        camera.setPosition(-6, 4, 6);
-        camera.lookAt(0, 1, 0);
-        app.root.addChild(camera);
+        this.entity.setPosition(-6, 4, 6);
+        this.entity.lookAt(0, 1, 0);
+        app.root.addChild(this.entity);
 
-        return camera;
     }
 
-    var camera = new Camera();
+    Camera.prototype.reset = function (dt) {
+        this.entity.setPosition(-6, 4, 6);
+        this.entity.lookAt(0, 1, 0);
+    };
+
+    Camera.prototype.update = function (dt) {
+        var camera = this.entity;
+        if (camera.getPosition().y < (4 + (p.boxHeight * model.boxs.length))) {
+            camera.setPosition(camera.getPosition().x, camera.getPosition().y + 0.5 * dt, camera.getPosition().z);
+        }
+    };
+    Camera.prototype.gameOverZoom = function (dt, callback) {
+        var camera = this.entity;
+        if (camera.getPosition().z < 10) {
+            camera.setPosition(camera.getPosition().x - 3 * dt, camera.getPosition().y + 3 * dt, camera.getPosition().z + 3 * dt);
+        } else {
+            callback && callback();
+        }
+    };
+    model.camera = new Camera();
     //endregion
 
     //region 灯光
     function Light() {
         var light = new pc.Entity();
-        light.addComponent('light');
+        light.addComponent('light', {
+            color: new pc.Color(1, 1, 1)
+        });
         light.setEulerAngles(45, 0, 30);
         app.root.addChild(light);
 
@@ -302,60 +386,83 @@ function init() {
     var light = new Light();
     var light = new Light2();
     var light = new Light2();
+    var light = new Light2();
     //endregion
 
-    //背景
-
     //验证游戏失败
-    checkGameOverFn();
-    function checkGameOverFn() {
-        var interval = setInterval(function () {
-            for (var i = 0; i < boxs.length; i++) {
-                var y = boxs[i].entity.getPosition().y;
-                if (y < (boxs[i].index * p.boxHeight)) {
-                    clearInterval(interval)
-                    GameOverFn();
-                    console.log('gameover')
-                }
-            }
-        }, 300);
-    }
-
-    //游戏结束
-    function GameOverFn() {
-        // app.isPaused=true;
-        for (var i = 0; i < boxs.length; i++) {
-            // boxs[i].animation.speed = 0;
-            boxs[i].entity.rigidbody.type = 'static';
-        }
-        p.isGameOver = true;
-
-        model.aimBox.entity.enabled = false;
-
-        getScoreFn();
-    }
-
-    //获取得分
-    function getScoreFn() {
-        console.log("得分：" + boxs.length)
-    }
-
+    this.checkGameOver();
 
     //刷新
-    var aimBoxSpeed = 5;
     app.on('update', function (dt) {
         if (p.isGameOver) {
-            if (camera.getPosition().z < (6 * 5)) {
-                camera.setPosition(camera.getPosition().x - 3 * dt, camera.getPosition().y, camera.getPosition().z + 3 * dt);
-            }
+            model.camera.gameOverZoom(dt);
         } else {
-            if (camera.getPosition().y < (4 + (p.boxHeight * boxs.length))) {
-                camera.setPosition(camera.getPosition().x, camera.getPosition().y + 0.5 * dt, camera.getPosition().z);
-            }
-
-            model.aimBox.update(dt);
+            model.camera.update(dt);
+            model.AimBox.update(dt);
         }
     });
 
-    return p;
 }
+Game.prototype.checkGameOver = function () {
+    var model = this.model;
+    var p = this.p;
+    var interval = setInterval(function () {
+        for (var i = 0; i < model.boxs.length; i++) {
+            var y = model.boxs[i].entity.getPosition().y;
+            if (y < (model.boxs[i].index * p.boxHeight - 1)) {
+                clearInterval(interval);
+
+                GameOverFn();
+                function GameOverFn() {
+                    //固定堆叠位置
+                    for (var i = 0; i < model.boxs.length; i++) {
+                        model.boxs[i].entity.rigidbody.type = 'static';
+                    }
+                    p.isGameOver = true;
+                    model.AimBox.entity.enabled = false;
+
+                    //得分画面
+                    $('.score-area').show();
+                    $('.score-number').html(model.boxs.length)
+
+                    console.log('gameover')
+                }
+
+            }
+        }
+    }, 300);
+};
+Game.prototype.restart = function () {
+    //清除已经堆叠的
+    var boxs = this.model.boxs;
+    for (var i = 0; i < boxs.length; i++) {
+        boxs[i].entity.destroy();
+    }
+    boxs.length = 0;
+
+    //
+    var AimBox = this.model.AimBox;
+    AimBox.reset();
+
+    //镜头
+    var camera = this.model.camera;
+    camera.reset();
+
+    //重置状态
+    this.p.isGameOver = false;
+    this.p.isCollision = true;
+
+    //清除计分牌
+    $('.score-area').hide();
+
+    this.checkGameOver();
+
+};
+
+$(function () {
+    var game = new Game();
+
+    $('.restart-btn').click(function () {
+        game.restart();
+    })
+});
